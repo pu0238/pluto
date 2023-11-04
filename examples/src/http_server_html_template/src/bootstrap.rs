3edf607f1b8c9a9873ff08e91f432aa786e98c36
Reyth3 <mrreynevan2@gmail.com>
2023-11-04 08:48:22 +0100

    feat: static files support with mime types

--- examples/src/http_server_html_template/src/bootstrap.rs
+++ examples/src/http_server_html_template/src/bootstrap.rs
@@ -1,10 +1,13 @@
 use ic_cdk_macros::{post_upgrade, query, update};
 use pluto::{
-    http::{HttpServe, RawHttpRequest, RawHttpResponse},
+    http::{HttpResponse, HttpServe, RawHttpRequest, RawHttpResponse},
     http_serve,
     router::Router,
 };
-use std::cell::RefCell;
+use std::{
+    cell::{RefCell, RefMut},
+    collections::HashMap,
+};
 
 use crate::controller;
 
@@ -12,10 +15,28 @@ thread_local! {
     static ROUTER: RefCell<Router>  = RefCell::new(controller::setup());
 }
 
+fn use_statics(mut router: RefMut<Router>) {
+    for file in crate::compiled::templates::statics::STATICS.iter() {
+        router.get(&format!("/static/{}", file.name), false, |_req| async {
+            let bytes = file.content.clone();
+            let mut headers: HashMap<String, String> = HashMap::new();
+            headers.insert("Content-Type".to_string(), file.mime.to_string());
+            return Ok(HttpResponse {
+                status_code: 200,
+                headers,
+                body: pluto::http::HttpBody::String(String::from_utf8(bytes.to_vec()).unwrap()),
+            });
+        });
+    }
+}
+
 // System functions
 #[post_upgrade]
 fn post_upgrade() {
-    ROUTER.with(|r| *r.borrow_mut() = controller::setup())
+    ROUTER.with(|r| {
+        *r.borrow_mut() = controller::setup();
+        use_statics(r.borrow_mut());
+    })
 }
 
 // Http interface
