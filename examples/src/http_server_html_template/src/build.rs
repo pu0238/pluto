45d2fb5c25e21f237a53a3bf5f37556cf59165e9
pu0238 <michal.lustyk>
2024-03-19 19:13:50 +0100

    Updated deps

--- examples/src/http_server_html_template/src/build.rs
+++ examples/src/http_server_html_template/src/build.rs
@@ -1,3 +1,11 @@
+use std::path::Path;
+
 pub fn main() -> Result<(), pluto_templating::RucteError> {
-    pluto_templating::initialize_templating_engine("views")
+    let views_dir = "views/";
+    let static_dir = "static/";
+    
+    let out_dir = std::env::var("OUT_DIR").expect("No source path set.");
+    let out_path = Path::new(&out_dir);
+
+    pluto_templating::initialize_templating_engine(out_path.to_path_buf(), views_dir, static_dir)
 }
